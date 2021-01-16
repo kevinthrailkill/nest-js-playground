496cae15ff3691e3977f21fe865941fc0727ed88
Kevin Thrailkill <kevinjt92@me.com>
2021-01-16 14:42:21 -0800

    Section 2: Postgres DB

---
+++ src/coffees/entities/coffee.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity() // sql table === 'coffee'
+export class Coffee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  brand: string;
+
+  @Column({ default: 0 })
+  recommendations: number;
+
+  @JoinTable() // 👈 Join the 2 tables - only the OWNER-side does this
+  @ManyToMany(
+    (type) => Flavor,
+    (flavor) => flavor.coffees, // what is "coffee" within the Flavor Entity
+    {
+      cascade: true, // ['insert']
+    },
+  ) // 👈
+  flavors: Flavor[];
+}
