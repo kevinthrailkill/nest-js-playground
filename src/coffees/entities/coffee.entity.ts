abcd11a7b4f985a371e9ee1a7fa1b8eae148f597
Kevin Thrailkill <kevinjt92@me.com>
2021-01-19 08:20:11 -0800

    change to mongo

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,20 @@
-export class Coffee {
-  id: number;
+import { Schema, Prop } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   brand: string;
+
+  @Prop({ default: 0 })
+  recommendations: number;
+
+  @Prop([String])
   flavors: string[];
 }
+
+export const CoffeeSchema = SchemaFactory.createForClass(Coffee);
